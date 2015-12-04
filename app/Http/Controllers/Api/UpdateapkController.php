e8979ea3fd30caded00fc400a1e4f019ca057ad7
Rencie Bautista <rencie.bautista@yahoo.com>
2015-12-04 13:01:40 +0800

    update csrftoken

--- app/Http/Controllers/Api/UpdateapkController.php
+++ app/Http/Controllers/Api/UpdateapkController.php
@@ -24,10 +24,27 @@ class UpdateapkController extends Controller
     }
 
     public function download(Request $request){
-        // $entry = Fileentry::where('file_id', '=', $fileId)->firstOrFail();
-        // $pathToFile=storage_path()."/app/".$entry->filename;
-        $pathToFile=storage_path()."/apk/app-debug.apk";
-        return response()->download($pathToFile);
+        // // $entry = Fileentry::where('file_id', '=', $fileId)->firstOrFail();
+        // // $pathToFile=storage_path()."/app/".$entry->filename;
+        $pathToFile = storage_path()."/apk/app-debug.apk";
+        // // return response()->download($pathToFile);
+        // return  response()->download($pathToFile, 'app-debug.apk', ['Content-Type: text/cvs']);
 
+
+        $file = $pathToFile; //not public folder
+        if (file_exists($file)) {
+            header('Content-Description: File Transfer');
+            header('Content-Type: application/vnd.android.package-archive');
+            header('Content-Disposition: attachment; filename='.basename($file));
+            header('Content-Transfer-Encoding: binary');
+            header('Expires: 0');
+            header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
+            header('Pragma: public');
+            header('Content-Length: ' . filesize($file));
+            ob_clean();
+            flush();
+            readfile($file);
+            exit;
+        }
     }
 }
