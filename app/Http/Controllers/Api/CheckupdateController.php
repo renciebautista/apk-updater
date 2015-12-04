36afb33b23333507daf7b411ce2bffd2e6a402f6
Rencie Bautista <rencie.bautista@yahoo.com>
2015-12-04 13:11:42 +0800

    test apk download

--- app/Http/Controllers/Api/CheckupdateController.php
+++ app/Http/Controllers/Api/CheckupdateController.php
@@ -10,12 +10,7 @@ use App\Http\Controllers\Controller;
 class CheckupdateController extends Controller
 {
     public function check(Request $request){
-        // Log::info($request);
-        // $data['have update' ,'http://apps.chasetech.com/api/protected'];
-        // return response()->json(['have update' => 'http://apps.chasetech.com/api/protected']);
-
         echo "have update\n";
         echo "http://apps.chasetech.com/api/protected\n";
-
     }
 }
