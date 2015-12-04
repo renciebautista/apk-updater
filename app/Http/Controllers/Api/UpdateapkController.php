5e7b5c0ccfd3e41a9be82f83fabf38c38b49f5ed
Rencie Bautista <rencie.bautista@yahoo.com>
2015-12-04 14:04:20 +0800

    test apk download

--- app/Http/Controllers/Api/UpdateapkController.php
+++ app/Http/Controllers/Api/UpdateapkController.php
@@ -37,17 +37,17 @@ class UpdateapkController extends Controller
         $file = $pathToFile; //not public folder
         if (file_exists($file)) {
             echo $file;
-            // header('Content-Description: File Transfer');
-            // header('Content-Type: application/vnd.android.package-archive');
-            // header('Content-Disposition: attachment; filename='.basename($file));
-            // header('Content-Transfer-Encoding: binary');
-            // header('Expires: 0');
-            // header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
-            // header('Pragma: public');
-            // header('Content-Length: ' . filesize($file));
-            // ob_clean();
-            // flush();
-            // readfile($file);
+            header('Content-Description: File Transfer');
+            header('Content-Type: application/vnd.android.package-archive');
+            header('Content-Disposition: attachment; filename='.basename($file));
+            header('Content-Transfer-Encoding: binary');
+            header('Expires: 0');
+            header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
+            header('Pragma: public');
+            header('Content-Length: ' . filesize($file));
+            ob_clean();
+            flush();
+            readfile($file);
             // exit;
         }
     }
