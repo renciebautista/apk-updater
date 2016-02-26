f7570dbf0de58e031705768ee4fedae0c2dadcf3
Rencie Bautista <rencie.bautista@yahoo.com>
2016-02-26 15:55:19 +0800

    test update

--- app/Http/Controllers/Api/CheckupdateController.php
+++ app/Http/Controllers/Api/CheckupdateController.php
@@ -16,21 +16,21 @@ class CheckupdateController extends Controller
     	$version = $request->input('version', 0);
     	$filemd5 = $request->input('md5', 0);
     	$deviceid = $request->id;
-        dd($request->all());
-    	// $apk = Apk::where('pkgname', $pkgname)
-     //        // ->where('version', '>', $version )
-     //        ->get();
 
-    	// if(!empty($apk)){
-    	// 	if($apk->version > $version){
-    	// 		echo "have update\n";
-     //    		// echo "http://apps.chasetech.com/api/protected/app-debug.apk\n";
-     //            echo action('Api\UpdateapkController@download', array('token' => $apk->token, 'filename' => $apk->filename))."\n";
-    	// 	}else{
-    	// 		echo "no update available\n";
-    	// 	}
-    	// }else{
-    	// 	echo "apk not found\n";
-    	// }
+    	$apk = Apk::where('pkgname', $pkgname)
+            ->where('version', '>', $version)
+            ->first();
+        print_r($apk);
+    	if(!empty($apk)){
+    		if($apk->version > $version){
+    			echo "have update\n";
+        		// echo "http://apps.chasetech.com/api/protected/app-debug.apk\n";
+                echo action('Api\UpdateapkController@download', array('token' => $apk->token, 'filename' => $apk->filename))."\n";
+    		}else{
+    			echo "no update available\n";
+    		}
+    	}else{
+    		echo "apk not found\n";
+    	}
     }
 }
